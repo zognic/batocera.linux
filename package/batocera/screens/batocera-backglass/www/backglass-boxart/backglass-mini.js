54a3fd8704667b348d4ce90b3b54deaea7651b8f
Modhack <modhack@batocera.org>
2025-09-26 19:14:12 +0200

    fix: Boxart backglass & image ratio

--- package/batocera/screens/batocera-backglass/www/backglass-boxart/backglass-mini.js
+++ package/batocera/screens/batocera-backglass/www/backglass-boxart/backglass-mini.js
@@ -1,11 +1,11 @@
 function onSystem(infos) {
     var html = "";
     if(infos["logo"]) {
-	html += "<div id=\"system_logo\"><div id=\"system_logo_internal\"><img src=\"" + infos["logo"] + "\" /></div></div>";
+    html += "<div id=\"system_logo\"><div id=\"system_logo_internal\"><img src=\"" + infos["logo"] + "\" /></div></div>";
     } else {
-	if(infos["fullname"]) {
-	    html += "<div id=\"system_text\"><div id=\"system_text_internal\">" + infos["fullname"] + "</div></div>";
-	}
+    if(infos["fullname"]) {
+        html += "<div id=\"system_text\"><div id=\"system_text_internal\">" + infos["fullname"] + "</div></div>";
+    }
     }
     document.getElementById("infos").innerHTML = html;
 }
@@ -13,12 +13,12 @@ function onSystem(infos) {
 function onGame(infos) {
     var html = "";
 
-    if(infos["boxart"]) {
-	html += "<div id=\"game_boxart\"><div id=\"game_boxart_internal\"><img src=\"" + infos["boxart"] + "\" /></div></div>";
+    if(infos["thumbnail"]) {
+    html += "<div id=\"game_boxart\"><div id=\"game_boxart_internal\"><img src=\"" + infos["thumbnail"] + "\" /></div></div>";
     } else {
-	if(infos["name"]) {
-	    html += "<div id=\"game_text\"><div id=\"game_text_internal\">" + infos["name"] + "</div></div>";
-	}
+    if(infos["name"]) {
+        html += "<div id=\"game_text\"><div id=\"game_text_internal\">" + infos["name"] + "</div></div>";
+    }
     }
     document.getElementById("infos").innerHTML = html;
 }
